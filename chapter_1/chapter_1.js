db1e0586e305d4f132ae671857d6b445fcc70339
Amy Huang <a3315520@gmail.com>
2020-02-16 16:34:50 +0800

    Step2-4: remove parameter

--- chapter_1/chapter_1.js
+++ chapter_1/chapter_1.js
@@ -3,7 +3,7 @@ function statement(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
     switch (playFor(aPerformance).type) {
     case "tragedy":
@@ -35,7 +35,7 @@ function statement(invoice, plays) {
   }).format
 
   for (const perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf))
+    let thisAmount = amountFor(perf)
 
     // 加入 volume credit
     volumeCredits += Math.max(perf.audience - 30, 0)
