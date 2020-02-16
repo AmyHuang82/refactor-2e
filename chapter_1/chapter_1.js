d139825bc5ba8e86242839e6318aead714857ae6
Amy Huang <a3315520@gmail.com>
2020-02-16 16:48:42 +0800

    Step3-1: add volumeCreditsFor function

--- chapter_1/chapter_1.js
+++ chapter_1/chapter_1.js
@@ -25,6 +25,13 @@ function statement(invoice, plays) {
     return result;
   }
 
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    return volumeCredits;
+  }
+
   let totalAmount = 0
   let volumeCredits = 0
   let result = `Statement for ${invoice.customer}\n`
@@ -35,10 +42,7 @@ function statement(invoice, plays) {
   }).format
 
   for (const perf of invoice.performances) {
-    // 加入 volume credit
-    volumeCredits += Math.max(perf.audience - 30, 0)
-    // 每十名喜劇觀眾可獲得額外分數
-    if (playFor(perf).type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
+    volumeCredits += volumeCreditsFor(perf)
 
     // 印出這筆訂單
     result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`
