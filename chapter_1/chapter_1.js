e9b7a98f068b727c226c4d3240292d98933ac034
Amy Huang <a3315520@gmail.com>
2020-02-16 20:24:28 +0800

    Step5-1: split loop and slide statements

--- chapter_1/chapter_1.js
+++ chapter_1/chapter_1.js
@@ -42,17 +42,19 @@ function statement(invoice, plays) {
   }
 
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (const perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // 印出這筆訂單
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
+  }
+
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
   }
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
