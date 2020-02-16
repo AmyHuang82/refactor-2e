01519a99dd71950f2fab9d2fdbf5c22b5d93e442
Amy Huang <a3315520@gmail.com>
2020-02-16 20:59:42 +0800

    Step7-1: extract function

--- chapter_1/chapter_1.js
+++ chapter_1/chapter_1.js
@@ -1,4 +1,8 @@
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -58,17 +62,14 @@ function statement(invoice, plays) {
   }
 
   let result = `Statement for ${invoice.customer}\n`;
-
   for (const perf of invoice.performances) {
     // 印出這筆訂單
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
   }
-
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
-
   return result;
 }
 
