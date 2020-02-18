6c8b4e900b5104c1de6dcd0c84b0cc1e5e6f6569
Amy Huang <a3315520@gmail.com>
2020-02-18 23:28:01 +0800

    Step7-10: split function

--- chapter_1/chapter_1.js
+++ chapter_1/chapter_1.js
@@ -5,14 +5,6 @@ function statement(invoice, plays) {
 }
 
 function renderPlainText(data) {
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format(aNumber / 100);
-  }
-
   let result = `Statement for ${data.customer}\n`;
   for (const perf of data.performances) {
     // 印出這筆訂單
@@ -23,6 +15,14 @@ function renderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber / 100);
 }
 
 module.exports = { statement };
